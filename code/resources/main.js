ab9ce319572d4345177a8abe8c05fed8ce31e219
vitaliy-js <133413126+vitaliy-js@users.noreply.github.com>
2024-11-17 18:05:53 +0200

    Add v.1.0

---
+++ code/resources/main.js
@@ -0,0 +1,153 @@
+export const main = [
+    {
+        name: 'Кратко про курс',
+        article: ['./about.html', './plan.html']
+    },
+    {
+        name: 'Взаимодействие с HTML',
+        test: ['./test.html?id=1','./test.html?id=2'],
+        practice: ['./practice.html?id=1'],
+        challenge: ['./challenge.html?id=1'],
+        materials: ['chapter_2.zip'],
+        video: ['https://youtu.be/6fNE-lkVxsY', 'https://youtu.be/hCcSQPMAgzw']
+    },
+    {
+        name: 'Инструкция if и функции',
+        test: ['./test.html?id=3','./test.html?id=4'],
+        practice: ['./practice.html?id=2','./practice.html?id=3'],
+        challenge: ['./challenge.html?id=2'],
+        materials: ['chapter_3.zip'],
+        video: ['https://youtu.be/7_AWoA8bEMM', 'https://youtu.be/-3JLLvGnHGE']
+    },
+    {
+        name: 'События в JavaScript',
+        test: ['./test.html?id=5','./test.html?id=6'],
+        practice: ['./practice.html?id=4'],
+        challenge: ['./challenge.html?id=3'],
+        materials: ['chapter_4.zip'],
+        video: ['https://youtu.be/PuNkXO7O-Vc']
+    },
+    {
+        name: 'Работа с DOM',
+        test: ['./test.html?id=7','./test.html?id=8'],
+        practice: ['./practice.html?id=5'],
+        challenge: ['./challenge.html?id=4'],
+        materials: ['chapter_5.zip'],
+        video: ['https://youtu.be/MZFBa3VBKuU']
+    },
+    {
+        name: 'Массивы',
+        test: ['./test.html?id=9','./test.html?id=10'],
+        practice: ['./practice.html?id=6'],
+        challenge: ['./challenge.html?id=5'],
+        materials: ['chapter_6.zip'],
+        video: ['https://youtu.be/W1zptYZhw8k']
+    },
+    {
+        name: 'Строковые данные',
+        test: ['./test.html?id=11','./test.html?id=12'],
+        practice: ['./practice.html?id=7'],
+        challenge: ['./challenge.html?id=6'],
+        materials: ['chapter_7.zip'],
+        video: ['https://youtu.be/IbY9Gtn98AE']
+    },
+    {
+        name: 'Методы чисел',
+        test: ['./test.html?id=13'],
+        practice: ['./practice.html?id=8'],
+        challenge: ['./challenge.html?id=7'],
+        materials: ['chapter_8.zip'],
+        video: ['https://youtu.be/AtT1HyB46ro']
+    },
+    {
+        name: 'Работа с localStorage',
+        test: ['./test.html?id=14'],
+        practice: ['./practice.html?id=9'],
+        challenge: ['./challenge.html?id=8'],
+        materials: ['chapter_9.zip'],
+        video: ['https://youtu.be/Zrpy0B3dYJA']
+    },
+    {
+        name: 'Методы массивов',
+        test: ['./test.html?id=15','./test.html?id=16'],
+        practice: ['./practice.html?id=10'],
+        challenge: ['./challenge.html?id=9'],
+        materials: ['chapter_10.zip'],
+        video: ['https://youtu.be/AZ6AEz37MNo']
+    },
+    {
+        name: 'Объекты',
+        test: ['./test.html?id=17','./test.html?id=18'],
+        practice: ['./practice.html?id=11'],
+        challenge: ['./challenge.html?id=10'],
+        materials: ['chapter_11.zip'],
+        video: ['https://youtu.be/lzfozZzK5Vw']
+    },
+    {
+        name: 'Дата и время',
+        test: ['./test.html?id=19'],
+        practice: ['./practice.html?id=12'],
+        challenge: ['./challenge.html?id=11'],
+        materials: ['chapter_12.zip'],
+        video: ['https://youtu.be/ungCP5xw1MY']
+    },
+    {
+        name: 'HTTP-протокол',
+        test: ['./test.html?id=20','./test.html?id=21'],
+        challenge: ['./challenge.html?id=12'],
+        materials: ['chapter_13.zip'],
+        video: ['https://youtu.be/D5zoBVYdTlY', 'https://youtu.be/TG5TAB3zVAs']
+    },
+    {
+        name: 'Асинхронный JavaScript',
+        test: ['./test.html?id=22'],
+        practice: ['./practice.html?id=13'],
+        challenge: ['./challenge.html?id=13'],
+        materials: ['chapter_14.zip'],
+        video: ['https://youtu.be/cFf3_4yQrpo', 'https://youtu.be/6jWQ9_wsKgM']
+    },
+    {
+        name: 'Регулярные выражения',
+        test: ['./test.html?id=23','./test.html?id=24'],
+        practice: ['./practice.html?id=14'],
+        challenge: ['./challenge.html?id=14'],
+        materials: ['chapter_15.zip'],
+        video: ['https://youtu.be/Bz_UhK6to3w']
+    },
+    {
+        name: 'Разное',
+        test: ['./test.html?id=25','./test.html?id=26'],
+        practice: ['./practice.html?id=15'],
+        challenge: ['./challenge.html?id=15'],
+        materials: ['chapter_16.zip'],
+        video: ['https://youtu.be/c2qgD60UVW8']
+    },
+    {
+        name: 'Базовый HTML',
+        test: ['./test.html?id=27','./test.html?id=28'],
+        practice: ['./practice.html?id=16'],
+        challenge: ['./challenge.html?id=16'],
+        materials: ['chapter_17.zip'],
+        video: ['https://youtu.be/_XtBfazbC6s']
+    },
+    {
+        name: 'Базовый CSS',
+        test: ['./test.html?id=29','./test.html?id=30'],
+        practice: ['./practice.html?id=17'],
+        challenge: ['./challenge.html?id=17'],
+        materials: ['chapter_18.zip'],
+        video: ['https://youtu.be/H4GNcJXFrdw', 'https://youtu.be/Dx8YZiaRvj4']
+    },
+    {
+        name: 'Работа с Git и GitHub',
+        test: ['./test.html?id=31'],
+        challenge: ['./challenge.html?id=18'],
+        materials: ['chapter_19.zip'],
+        video: ['https://youtu.be/oPkHoW4wP88']
+    },
+    {
+        name: 'Заключение',
+        materials: ['chapter_20.zip'],
+        video: ['https://youtu.be/CPs37Ei7Als', 'https://youtu.be/PygIXS1i2do', 'https://youtu.be/9vB1Mv0Zz14']
+    }
+];
